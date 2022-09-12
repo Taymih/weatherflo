c3542ed661014479ed0016b03561e164ce6fc05e
Aderemi Temilola <otaymih16@gmail.com>
2022-09-12 16:50:27 -0500

    Added daily weather api

--- src/app.js
+++ src/app.js
@@ -43,15 +43,20 @@ function displayFigures(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
   celsiusTemperature = response.data.main.temp;
-  displayForecast();
+  checkForecast(response.data.coord);
 }
-
-function displayForecast() {
+function checkForecast(coordinates) {
+  apiKey = "628687b1313ed233e8a7594970069fef";
+  apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
+};
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
 
-  let forecastHTML = `<div class="row mt-50">`;
+  let forecastHTML = `<div class="row">`;
   days.forEach(function (day) {
     forecastHTML =
       forecastHTML +
