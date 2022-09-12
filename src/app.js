0dee8cc6c2de79bde723c1a68718fb9be2580ecc
Aderemi Temilola <otaymih16@gmail.com>
2022-09-11 19:18:44 -0500

    Created search engine

--- src/app.js
+++ src/app.js
@@ -1,28 +1,34 @@
-let city = "New York";
+function displayFigures(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+    let skyElement = document.querySelector("#sky");
+    let iconElement = document.querySelector("#icon");
+    let cityName = document.querySelector("#city-name");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+    skyElement.innerHTML = response.data.weather[0].description;
+    cityName.innerHTML= response.data.name;
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+};
+function search(city) {
 let apiKey = "628687b1313ed233e8a7594970069fef";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayFigures);
+}
+search("Paris");
+function queryInput(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#enter");
+  console.log(cityInput.value);
+  search(cityInput.value);
+}
 
-function displayFigures(response) {
-    console.log(response.data);
-    let temperatureElement = document.querySelector("#temperature");
-    let humidityElement = document.querySelector("#humidity"); 
-    let windElement = document.querySelector("#wind");
-    let skyElement = document.querySelector("#sky");
-    let cityName = document.querySelector("#city-name");
-    let iconElement = document.querySelector("#icon");
-    temperatureElement.innerHTML = Math.round(response.data.main.temp);
-    humidityElement.innerHTML = response.data.main.humidity;
-    windElement.innerHTML = Math.round(response.data.wind.speed);
-    skyElement.innerHTML = response.data.weather[0].description;
-    cityName.innerHTML = city;
-    iconElement.setAttribute(
-      "src",
-      `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-    );
-    iconElement.setAttribute(
-      "alt",
-      response.data.weather[0].description,
-    );
-};
-
+let searchForm = document.querySelector("#search-panel");
+searchForm.addEventListener("submit", queryInput);
