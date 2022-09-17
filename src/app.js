ce1108df756563b6d6b5ab4ede34359e2689490c
Aderemi Temilola <otaymih16@gmail.com>
2022-09-17 18:04:17 -0500

    Added API integration for forecast

--- src/app.js
+++ src/app.js
@@ -52,38 +52,48 @@ function displayFigures(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
   celsiusTemperature = response.data.main.temp;
-  checkForecast(response.data.coords);
-}
+  checkForecast(response.data.coord);
+};
+
 function checkForecast(coordinates) {
-  let apiKey = "628687b1313ed233e8a7594970069fef";
-  let apiUrl = ;
+  let apiKey = "8161b4309ee03faae957729ba7104797";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`; 
   axios.get(apiUrl).then(displayForecast);
 };
 
-function displayForecast() {
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thurs"];
+
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-    forecastHTML = forecastHTML + `
+  forecast.forEach(function (forecastData, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
                     <div class="col-2">
                         <div class="weather-forecast-date">
-                            ${day}
+                            ${formatDay(forecastData.dt)}
                         </div>
-                        <img src="#" alt="#" width="42"/>
+                        <img src="http://openweathermap.org/img/wn/${forecastData.weather[0].icon}@2x.png" 
+                        alt="#" 
+                        width="42"
+                        />
                         <div class="weather-temperatures">
-                            <span class="weather-forecast-temperatures-max">24°</span>
-                            <span class="weather-forecast-temperatures-min">18°</span>
+                            <span class="weather-forecast-temperatures-max">${Math.round(forecastData.temp.max)}</span>
+                            <span class="weather-forecast-temperatures-min">${Math.round(forecastData.temp.min)}</span>
                         </div>
                     </div>
                     `;
+    };
   });
   forecastHTML = forecastHTML + `</div>`;
     forecastElement.innerHTML = forecastHTML;
 };
   
 function search(city) {
-  let apiKey = "628687b1313ed233e8a7594970069fef";
+  let apiKey = "8161b4309ee03faae957729ba7104797";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayFigures);
 }
@@ -112,7 +122,7 @@ function celsiusFigure(event) {
 }
 
 function searchLocation(position) {
-  let apiKey = "628687b1313ed233e8a7594970069fef";
+  let apiKey = "8161b4309ee03faae957729ba7104797";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayFigures);
 }
